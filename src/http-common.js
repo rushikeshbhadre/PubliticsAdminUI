66eaf58fa16774e8184bfeea38e5eb457bfe34ed
rushikeshbhadre <rushikeshbhadre1@gmail.com>
2022-07-27 00:53:51 +0530

    replaced localhost with ip

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://139.59.72.105:8080/api",
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
